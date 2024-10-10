ead21c38c287f18e14c12da4d6bb50ab6436af85
cutie-sudo <faith.nguli@student.moringaschool.com>
2024-10-10 13:43:39 +0300

    add phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+//Remove the <main> element
+const main = document.getElementById("main");
+main.remove();
+
+//Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+//Set the id of the new header
+newHeader.id = "victory";
+
+newHeader.textContent = "YOUR-NAME is the Champion;"
+
+document.body.appendChild(newHeader);
+
+
+
+
+
+
+
